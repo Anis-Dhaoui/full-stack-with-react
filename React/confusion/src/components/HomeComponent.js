016cbebede8e5abf2a74dc578f88edc167385d2e
Anis-Dhaoui <freeconcept6@gmail.com>
2020-07-17 00:38:25 +0100

    SPA Part 1

--- React/confusion/src/components/HomeComponent.js
+++ React/confusion/src/components/HomeComponent.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import RenderCard from './RenderComponent';
 
 function Home (props) {
     return(
-        <div>
-          <h4>Homexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</h4>
+        <div className="container">
+          <div className="row justify-content-start">
+            <div className="col-12 col-md m-1">
+              <RenderCard item={props.dish} />
+            </div>
+            <div className="col-12 col-md m-1">
+              <RenderCard item={props.promo} />
+            </div>
+            <div className="col-12 col-md m-1">
+              <RenderCard item={props.leader} />
+            </div>
+
+          </div>
         </div>
     );
 };
--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -5,7 +5,12 @@ import Menu from "./MenuComponent";
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import {DISHES} from "../shared/dishesx";
+import Contact from './ContactComponent';
+import { DISHES } from "../shared/dishesx";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
+
 
 class Main extends Component {
   
@@ -13,14 +18,19 @@ class Main extends Component {
     super(props);
       this.state = {
         dishes: DISHES,
-        selectedDish: null
+        comments: COMMENTS,
+        leaders: LEADERS,
+        promotions: PROMOTIONS
       };
   };
 
   render(){
     const homePage = () =>{
       return(
-        <Home />
+        <Home dish={this.state.dishes.filter((dishItem) => dishItem.featured)[0]}
+              promo={this.state.promotions.filter((promoItem) => promoItem.featured)[0]} 
+              leader={this.state.leaders.filter((leaderItem) => leaderItem.featured)[0]}
+        />
       );
     };
   return (
@@ -30,6 +40,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={homePage} />
           <Route exact path="/menu" component={() => <Menu dishes2={this.state.dishes} />} />
+          <Route path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
       </div>
