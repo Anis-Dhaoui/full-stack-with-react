2f37cc7e781cbd98f1f1d280a68e3e25f49da710
Anis-Dhaoui <freeconcept6@gmail.com>
2020-08-13 02:26:35 +0100

    Copying The Build Folder To The Public Folder in Json-Server Folder Therefore our json-server can access to it

---
+++ json-server/public/precache-manifest.0d181de28b3b26b6ad0d96198b9fe2be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326739a9a908ec05e2445bfdaeb7535b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f76714ea89c46ae5f0ea",
+    "url": "/static/css/2.c21e1fb8.chunk.css"
+  },
+  {
+    "revision": "182cf7a1e71baeb37162",
+    "url": "/static/css/main.cd73e3d1.chunk.css"
+  },
+  {
+    "revision": "f76714ea89c46ae5f0ea",
+    "url": "/static/js/2.facc7ebe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.facc7ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "182cf7a1e71baeb37162",
+    "url": "/static/js/main.3436cfa6.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
